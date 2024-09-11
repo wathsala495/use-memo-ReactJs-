11c1003f0dbb8c4773d5b86e06a50e6c86711049
wathsala495 <sumuduwathsala80@gmail.com>
2024-09-11 15:23:29 +0530

    use memo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMemo, useReducer, useRef, useState } from 'react';
 
+
+//use memo eka application eka speed karann bawitha karai(performane wadi karann)
+const inputhandle =(inputValue)=>{
+  let data;
+  for(let i=0;i<1000000000;i++){
+    data = i
+
+  }
+  console.log("inputHandle running")
+  return inputValue
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  const [imageUrl,setImageUrl]=useState('')
+  
+ //  const inputValue=inputhandle(imageUrl)
+  const imageData=useMemo(()=>inputhandle(imageUrl),[imageUrl])
+     return (
+    <div >
+     <input type='text' placeholder='Enter image url' onChange={(e)=>setImageUrl(e.target.value)} />
+     <input type='text' placeholder='Enter your name' />
+     <input type='text' placeholder='Enter your city' />
+     <input type='text' placeholder='Enter your position' />
+     <button >Set Data</button>
+    
+     <div>
+      <p>{imageUrl}</p>
+      
     </div>
+    </div>
+
+   
   );
 }
 
